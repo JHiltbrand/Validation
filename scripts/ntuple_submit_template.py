9a4d9cbff763d5306881fbb8bab996c3218642d0
Josh Hiltbrand <hiltbran@umn.edu>
2020-09-07 07:20:36 -0500

    get CRAB submission scripts up-to-date

--- scripts/ntuple_submit_template.py
+++ scripts/ntuple_submit_template.py
@@ -1,24 +1,22 @@
 # template for crab submission.
 # submit_jobs.py will insert definitions above
-conditionType = "def" # default
 # if new L1TriggerObjects conditons have been specified with either
 # a new tag or file
-if NEWCONDITIONS:
-    conditionType = "new_cond"
 
 from CRABClient.UserUtilities import config
 config = config()
 
-config.General.requestName = 'hcal' + '_' + str(PFA) + "_" + str(DATASET.split("/")[1]) + "_" + conditionType
+config.General.requestName = 'hcal' + '_' + str(PFA) + "_" + str(DATASET.split("/")[1])
 config.General.transferLogs = True
 config.General.transferOutputs = True
 
 # Name of the CMSSW configuration file                                                                                                       
-config.JobType.psetName = 'ntuple_maker_' + conditionType + '.py'
+config.JobType.psetName = 'ntuple_maker.py'
 config.JobType.allowUndistributedCMSSW = True
 
 config.JobType.pluginName = 'Analysis'
-#config.JobType.maxMemoryMB = 2500                                                                                                           
+config.JobType.maxMemoryMB = 2500
+config.JobType.sendPythonFolder = True
 config.JobType.outputFiles = ['L1Ntuple.root']
 
 config.Data.inputDataset = DATASET
@@ -29,11 +27,16 @@ config.Data.unitsPerJob = 1
 config.Data.useParent = USEPARENT
 
 # This string is used to construct the output dataset name                                                                                   
-config.Data.outputDatasetTag = 'Hcal' + '_' + str(PFA) + "_" + str(DATASET.split("/")[1]) + "_" + conditionType
+config.Data.outputDatasetTag = 'Hcal' + '_' + str(PFA) + "_" + str(DATASET.split("/")[1])
 
 # These values only make sense for processing data                                                                                           
 #    Select input data based on a lumi mask                                                                                                  
-#config.Data.lumiMask = LUMIMASK
+#config.Data.runRange = '322359-325273'
+#config.Data.runRange = '320394-322358'
+
+#config.Data.lumiMask = 'https://cms-service-dqm.web.cern.ch/cms-service-dqm/CAF/certification/Collisions18/13TeV/Era/Prompt/Cert_320394-325273_13TeV_PromptReco_Collisions18_JSON_eraD.txt'
+
+config.Data.lumiMask = LUMIMASK
 
 #    Select input data based on run-ranges                                                                                                   
 #config.Data.runRange = str(RUN)
--- scripts/submit_jobs.py
+++ scripts/submit_jobs.py
@@ -1,26 +1,8 @@
 #!/usr/bin/env python
 """Create CRAB submission script for L1 rate change 
 when L1TriggerObjects conditions are updated"""
-import sys
-import os
-import argparse
-import json
-
-## choose the type of jobs: use ['def', 'new_cond'] for Validation and ['def'] for other:
-COND_LIST = ['def']
-#COND_LIST = ['def', 'new_cond']
-# Relatively stable parameters can have defaults.
-# era should never change within a year
-ERA = 'Run3'
-# current data global tag
-CONDITIONS = '106X_mcRun3_2021_realistic_v3'
-# L1 calibrations; needs to be updated when L1 calibrations change
-CALOSTAGE2PARAMS = '2018_v1_3'
-# dummy value needed so that cmsDriver.py will
-# assume that there is an input file
-DEFAULTINPUT = '/store/express/Run2017B/ExpressPhysics/FEVT/Express-v1/000/297/562/00000/EE1F5F26-145B-E711-A146-02163E019C23.root'
-# frontier database (needs to be specified when overriding conditions)
-FRONTIER = 'frontier://FrontierProd/CMS_CONDITIONS'
+import subprocess, sys, os, argparse, json
+from algo_weights import pfaWeightsMap as pwm
 
 def check_setup():
     if not ("CMSSW_BASE" in os.environ):
@@ -28,165 +10,172 @@ def check_setup():
     if not ("crabclient" in os.environ['PATH']):
         sys.exit("Please set up crab environment before running")
 
-def generate_ntuple_config(configtype, newtag, caloparams, algo):
-    """Generates ntuple python file for a given 
-    config type (default or new conditions) and 
-    a new HcalL1TriggerObjects tag"""
+# Run cmsDriver to make cmsRun config for generating L1 ntuples
+def generate_ntuple_config(caloparams, globaltag, era, isData, needParent, useRECO, customize):
 
-    cmd = 'cmsDriver.py l1Ntuple -s RAW2DIGI '
-    cmd += '--python_filename=ntuple_maker_' + configtype + '.py '
-    # number of events; overridden by CRAB
-    cmd += '-n 10 '
+    commandList = ['cmsDriver.py', 'l1Ntuple', '-s', 'RAW2DIGI']
+
+    commandList.append('--python_filename=ntuple_maker.py')
+    commandList.append('-n'); commandList.append('-1')
+
     # suppresses the (unneeded) the RAW2DIGI output
-    cmd += '--no_output '
+    commandList.append('--no_output')
+
     # should always be set to Run2_2018 for 2018 data
-    cmd += '--era=' + ERA +  ' '
+    commandList.append('--era=%s'%(era))
+
     # validations are always run on data, not MC
-    cmd += '--mc '
+    if isData: commandList.append('--data')
+    else:      commandList.append('--mc')
+
     # default conditions
-    cmd += '--conditions=' + CONDITIONS + ' '
+    commandList.append('--conditions=%s'%(globaltag))
+
     # run re-emulation including re-emulation of HCAL TPs
-    cmd += '--customise=L1Trigger/Configuration/customiseReEmul.L1TReEmulFromRAWsimHcalTP '
+    commandList.append('--customise=L1Trigger/Configuration/customiseReEmul.L1TReEmulFromRAWsimHcalTP')
 
-    if ARGS.rates:
-        # include emulated quantities in L1Ntuple
-        cmd += '--customise=L1Trigger/L1TNtuples/customiseL1Ntuple.L1NtupleRAWEMU '
-    elif ARGS.effs:
-        # include emulated quantities in L1Ntuple
-        cmd += '--customise=L1Trigger/L1TNtuples/customiseL1Ntuple.L1NtupleAODRAWEMU '
+    # include emulated quantities in L1Ntuple
+    if useRECO:   commandList.append('--customise=L1Trigger/L1TNtuples/customiseL1Ntuple.L1NtupleAODRAWEMU')
+    else:         commandList.append('--customise=L1Trigger/L1TNtuples/customiseL1Ntuple.L1NtupleRAWEMU')
+
     # use correct CaloStage2Params; should only change if Layer2 calibration changes
-    if(caloparams):
-        cmd += '--customise=L1Trigger/Configuration/customiseSettings.L1TSettingsToCaloParams_' + CALOSTAGE2PARAMS + ' '
-    # override HcalL1TriggerObjects
-    if(configtype == 'new_cond'):
-        cmd += '--custom_conditions=' + newtag + ',HcalL1TriggerObjectsRcd,' + FRONTIER + ' '
+    if(caloparams): commandList.append('--customise=L1Trigger/Configuration/customiseSettings.L1TSettingsToCaloParams_%s'%(caloparams))
+
     # need to use LUTGenerationMode = False because we are using L1TriggerObjects
-    cmd += "--customise_commands='process.HcalTPGCoderULUT.LUTGenerationMode=cms.bool(False)' "
+    commandList.append("--customise_commands=process.HcalTPGCoderULUT.LUTGenerationMode=cms.bool(False)\\n"+customize)
+
     # default input file
+    commandList.append('--filein=CHILDFILE.root')
 
-    cmd += '--filein=' + DEFAULTINPUT + ' '
-    cmd += '--no_exec '
-    return cmd
+    if needParent: commandList.append('--secondfilein=PARENTFILE.root')
 
-PARSER = argparse.ArgumentParser()
-# existing GT
-PARSER.add_argument('-g', '--globaltag')
-# new L1TriggerObjects tag
-PARSER.add_argument('-t', '--newtag', required=False)
-#PARSER.add_argument('-l', '--lumimask', required=True)
-PARSER.add_argument('-d', '--dataset', required=True)
-PARSER.add_argument('-s', '--scheme', required=True)
-PARSER.add_argument('-o', '--outputsite', required=True)
-PARSER.add_argument('-p', '--useparent', default=False, action="store_true")
-PARSER.add_argument('-n', '--no_exec', default=False, action="store_true")
-PARSER.add_argument('-c', '--caloparams')
-PARSER.add_argument('--rates', default=False, action="store_true")
-PARSER.add_argument('--effs', default=False, action="store_true") 
-ARGS = PARSER.parse_args()
+    commandList.append('--no_exec')
+    commandList.append('--fileout=L1Ntuple.root')
 
-if ARGS.rates == False and ARGS.effs == False:
-    print "Must decide running rates or efficiencies"
-    print "Exiting..."
-    quit()
+    print "\n"
+    print "Running cmsDriver like this:\n"
+    print " ".join(commandList)
+    print "\n"
 
-# check environment setup
-check_setup()
+    subprocess.call(commandList)
 
-#FILE = file(ARGS.lumimask)
-#GOOD_RUN_STRING = FILE.read()
-#GOOD_RUN_DATA = json.loads(GOOD_RUN_STRING)
-if(ARGS.globaltag):
-    CONDITIONS = ARGS.globaltag
-if(ARGS.caloparams):
-    CALOSTAGE2PARAMS = ARGS.caloparams   
-#if len(GOOD_RUN_DATA) != 1:
-#    sys.exit("Only running on a single run at a time is supported.")
-#RUN = GOOD_RUN_DATA.keys()[0]
-# generate configs both for default and new conditions
-#for jobtype in ['def', 'new_cond']:
-if ARGS.newtag>0:
-    COND_LIST.append('new_cond') 
+# This function injects config code fragments into the cmsRun config that was generated
+# These things cannot be specified during the cmsDriver execution
+def customCommand(scheme, isData):
 
-for jobtype in COND_LIST:
+    #custom = 'process.SiteLocalConfigService = cms.Service("SiteLocalConfigService",overrideSourceCacheHintDir = cms.untracked.string("lazy-download"),)\\n'
+    custom = ''
+    if scheme == "PFA2": return custom 
+
+    custom += "process.load('SimCalorimetry.HcalTrigPrimProducers.hcaltpdigi_cff')\\n"
+    
+    if "PFA2p" in scheme:
+        custom += "process.simHcalTriggerPrimitiveDigis.numberOfPresamplesHBQIE11 = 1\\n"
+        custom += "process.simHcalTriggerPrimitiveDigis.numberOfPresamplesHEQIE11 = 1\\n"
+
+    elif "PFA1p" in scheme:
+        custom += "process.simHcalTriggerPrimitiveDigis.numberOfPresamplesHBQIE11 = 1\\n"
+        custom += "process.simHcalTriggerPrimitiveDigis.numberOfPresamplesHEQIE11 = 1\\n"
+        custom += "process.HcalTPGCoderULUT.contain1TSHB = True\\n"
+        custom += "process.HcalTPGCoderULUT.contain1TSHE = True\\n"
+        if isData:
+            custom += "process.HcalTPGCoderULUT.containPhaseNSHB = 0.0\\n"
+            custom += "process.HcalTPGCoderULUT.containPhaseNSHE = 0.0\\n"
+        else:
+            custom += "process.HcalTPGCoderULUT.containPhaseNSHB = 3.0\\n"
+            custom += "process.HcalTPGCoderULUT.containPhaseNSHE = 3.0\\n"
+
+    elif "PFA1" in scheme:
+        custom += "process.HcalTPGCoderULUT.contain1TSHB = True\\n"
+        custom += "process.HcalTPGCoderULUT.contain1TSHE = True\\n"
+        if isData:
+            custom += "process.HcalTPGCoderULUT.containPhaseNSHB = 0.0\\n"
+            custom += "process.HcalTPGCoderULUT.containPhaseNSHE = 0.0\\n"
+        else:
+            custom += "process.HcalTPGCoderULUT.containPhaseNSHB = 3.0\\n"
+            custom += "process.HcalTPGCoderULUT.containPhaseNSHE = 3.0\\n"
+
+    elif "PFAX1" in scheme:
+        custom += "process.simHcalTriggerPrimitiveDigis.numberOfPresamplesHEQIE11 = 1\\n"
+        custom += "process.HcalTPGCoderULUT.contain1TSHE = True\\n"
+        if isData: custom += "process.HcalTPGCoderULUT.containPhaseNSHE = 0.0\\n"
+        else:      custom += "process.HcalTPGCoderULUT.containPhaseNSHE = 3.0\\n"
+
+    elif "PFAX2" in scheme:
+        custom += "process.simHcalTriggerPrimitiveDigis.numberOfPresamplesHEQIE11 = 1\\n"
+
+    wStr = str(pwm[scheme])
+    wStr = wStr.replace("'", r'"')
+    custom += "process.simHcalTriggerPrimitiveDigis.weightsQIE11 = %s"%(wStr)
+
+    return custom
+
+if __name__ == '__main__':
+
+    JOB_TYPE = 'def'
+    
+    # Relatively stable parameters can have defaults.
+    # era should never change within a year
+    ERA = 'Run3'
+    
+    # current data global tag
+    CONDITIONS = '110X_mcRun3_2021_realistic_v6'
+    
+    # dummy value needed so that cmsDriver.py will
+    # assume that there is an input file
+    DEFAULTINPUT = '/store/express/Run2017B/ExpressPhysics/FEVT/Express-v1/000/297/562/00000/EE1F5F26-145B-E711-A146-02163E019C23.root'
+    
+    # frontier database (needs to be specified when overriding conditions)
+    FRONTIER = 'frontier://FrontierProd/CMS_CONDITIONS'
+
+    PARSER = argparse.ArgumentParser()
+    PARSER.add_argument('-g', '--globaltag', default='110X_mcRun3_2021_realistic_v6')
+    PARSER.add_argument('-t', '--tag', required=False)
+    PARSER.add_argument('-l', '--lumimask', required=True)
+    PARSER.add_argument('-d', '--dataset', required=True)
+    PARSER.add_argument('-s', '--scheme', required=True)
+    PARSER.add_argument('-o', '--outputsite', default="T3_US_FNALLPC")
+    PARSER.add_argument('-e', '--era', default="Run3")
+    PARSER.add_argument('-p', '--needParent', default=False, action="store_true")
+    PARSER.add_argument('-r', '--useRECO', default=False, action="store_true")
+    PARSER.add_argument('-n', '--no_exec', default=False, action="store_true")
+    PARSER.add_argument('-c', '--caloparams', default='2018_v1_3')
+    ARGS = PARSER.parse_args()
+    
+    isData = False
+    if "Run20" in ARGS.dataset: isData = True
+
+    if isData: 
+        CONDITIONS = "110X_dataRun2_v12"
+        ERA = "Run2_2018"
+    
+    # check environment setup
+    #check_setup()
+    
     tmpfile = 'submit_tmp.py'
     crab_submit_script = open(tmpfile, 'w')
-    #crab_submit_script.write("RUN = " + str(RUN) + '\n')
-    if jobtype == 'def':
-        crab_submit_script.write("NEWCONDITIONS = False\n")
-    else:
-        crab_submit_script.write("NEWCONDITIONS = True\n")
+
+    if JOB_TYPE == 'def': crab_submit_script.write("NEWCONDITIONS = False\n")
+    else:                crab_submit_script.write("NEWCONDITIONS = True\n")
+
     crab_submit_script.write("OUTPUTSITE = '" + ARGS.outputsite + "'\n")
-    #crab_submit_script.write("LUMIMASK = '" + ARGS.lumimask + "'\n")
+    crab_submit_script.write("LUMIMASK = '" + ARGS.lumimask + "'\n")
+    crab_submit_script.write("PFA = '" + ARGS.scheme + "'\n")
     crab_submit_script.write("DATASET = '" + ARGS.dataset + "'\n\n")
-    crab_submit_script.write("USEPARENT = %r"%(ARGS.useparent) + "\n\n")
-    crab_submit_script.write("PFA = '" + ARGS.scheme + "'\n\n")
+    crab_submit_script.write("USEPARENT = %r"%(ARGS.needParent) + "\n\n")
     crab_submit_script.close()
     
     # concatenate crab submission file with template
-    filename = 'submit_run_' + jobtype + '.py'
+    filename = 'submit_run_' + JOB_TYPE + '.py'
     command = "cat submit_tmp.py ntuple_submit_template.py > " + filename
     os.system(command)
     os.remove(tmpfile)
-
+    
+    custom = customCommand(ARGS.scheme, isData)
+    
     # generate cmsDriver commands
-    if ARGS.newtag>0:
-        print generate_ntuple_config(jobtype, ARGS.newtag, ARGS.caloparams, ARGS.scheme)
-        os.system(generate_ntuple_config(jobtype, ARGS.newtag, ARGS.caloparams, ARGS.scheme))
-    else:
-        print generate_ntuple_config(jobtype,0, ARGS.caloparams, ARGS.scheme)
-        os.system(generate_ntuple_config(jobtype, 0, ARGS.caloparams, ARGS.scheme))  
+    generate_ntuple_config(ARGS.caloparams, CONDITIONS, ERA, isData, ARGS.needParent, ARGS.useRECO, custom)
 
-    f = open("ntuple_maker_def.py", "r")
-    contents = f.readlines()
-    f.close()
-    
-    # Begin lamest hack in the world
-    contents.insert(2, "import sys\n")
-    contents.insert(3, "sys.path.insert(0, \"/uscms/home/jhiltb/nobackup/HCAL_Trigger_Study/scripts/\")\n")
-    contents.insert(10, "from algo_weights import pfaWeightsMap\n")
-
-    contents.insert(22,"process.load(\"SimCalorimetry.HcalTrigPrimProducers.hcaltpdigi_cff\")\n")
-    contents.insert(23,"\n")
-    
-    if "PFA2p" in ARGS.scheme:
-        contents.insert(24, "print \"Only using 3 samples\"\n")
-        contents.insert(25, "process.simHcalTriggerPrimitiveDigis.numberOfSamplesQIE11 = 3\n")
-        contents.insert(26, "process.simHcalTriggerPrimitiveDigis.numberOfPresamplesQIE11 = 1\n")
-        contents.insert(27, "process.HcalTPGCoderULUT.contain1TS = False\n")
-        contents.insert(28, "process.HcalTPGCoderULUT.containPhaseNS = 3.0\n")
-    
-    elif "PFA2" in ARGS.scheme:
-        contents.insert(24, "print \"Only using 2 samples\"\n")
-        contents.insert(25, "process.simHcalTriggerPrimitiveDigis.numberOfSamplesQIE11 = 2\n")
-        contents.insert(26, "process.simHcalTriggerPrimitiveDigis.numberOfPresamplesQIE11 = 0\n")
-        contents.insert(27, "process.HcalTPGCoderULUT.contain1TS = False\n")
-        contents.insert(28, "process.HcalTPGCoderULUT.containPhaseNS = 3.0\n")
-
-    elif "PFA1p" in ARGS.scheme:
-        contents.insert(24, "print \"Only using 2 sample\"\n")
-        contents.insert(25, "process.simHcalTriggerPrimitiveDigis.numberOfSamplesQIE11 = 2\n")
-        contents.insert(26, "process.simHcalTriggerPrimitiveDigis.numberOfPresamplesQIE11 = 1\n")
-        contents.insert(27, "process.HcalTPGCoderULUT.contain1TS = True\n")
-        contents.insert(28, "process.HcalTPGCoderULUT.containPhaseNS = 3.0\n")
-
-    elif "PFA1" in ARGS.scheme:
-        contents.insert(24, "print \"Only using 1 sample\"\n")
-        contents.insert(25, "process.simHcalTriggerPrimitiveDigis.numberOfSamplesQIE11 = 1\n")
-        contents.insert(26, "process.simHcalTriggerPrimitiveDigis.numberOfPresamplesQIE11 = 0\n")
-        contents.insert(27, "process.HcalTPGCoderULUT.contain1TS = True\n")
-        contents.insert(28, "process.HcalTPGCoderULUT.containPhaseNS = 3.0\n")
-
-    contents.insert(28,"\n")
-    contents.insert(29,"if \"%s\" in pfaWeightsMap:\n"%(ARGS.scheme))
-    contents.insert(30,"    process.simHcalTriggerPrimitiveDigis.weightsQIE11 = pfaWeightsMap[\"%s\"]\n"%(ARGS.scheme))
-    contents.insert(31,"else:\n")
-    contents.insert(32,"    print \"No weights defined for scheme '%s'; defaulting to zero weights!\"\n\n"%(ARGS.scheme))
-    
-    f = open("ntuple_maker_def.py", "w")
-    contents = "".join(contents)
-    f.write(contents)
-    f.close()
-
-    if(not ARGS.no_exec):
+    if not ARGS.no_exec:
         crabcmd = "crab submit " + filename
         os.system(crabcmd)
